a1d62e6f3c7d74a82be6ef3c3eb20ee986e467bb
minseg <artssam7602@naver.com>
2024-12-07 23:26:48 +0900

    repeller 코드 정리

--- repeller.js
+++ repeller.js
@@ -1,29 +1,23 @@
-let repeller = { pos: createVector(width / 4, height / 4), size: 30 };
+class Repeller {
+    constructor(x, y) {
+        this.pos = createVector(x, y);
+        this.size = 30;
+        this.strength = -50;
+    }
 
-function draw() {
-    background(240);
-
-    // Repeller 표시
-    fill(255, 0, 0, 150);
-    ellipse(repeller.pos.x, repeller.pos.y, repeller.size);
-
-    particles.forEach(p => {
-        // Repeller의 밀어내는 힘 계산
-        let dir = p.pos.copy().sub(repeller.pos);
+    repel(particle) {
+        let dir = particle.pos.copy().sub(this.pos);
         let distance = constrain(dir.mag(), 5, 100);
         dir.normalize();
-        let force = -50 / (distance * distance);
+        let force = this.strength / (distance * distance);
         dir.mult(force);
-        p.acc.add(dir);
+        return dir;
+    }
 
-        // 파티클 속도와 위치 업데이트
-        p.vel.add(p.acc);
-        p.pos.add(p.vel);
-        p.acc.mult(0);
-
-        // 파티클 그리기
+    show() {
         noStroke();
-        fill(150, 200, 255, 150);
-        ellipse(p.pos.x, p.pos.y, p.size);
-    });
+        fill(255, 0, 0, 150);
+        ellipse(this.pos.x, this.pos.y, this.size);
+    }
 }
+
