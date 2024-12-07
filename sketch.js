5dfd1bac82c7a32bd7f8bc024b91e5ceb72ca9dc
minseg <artssam7602@naver.com>
2024-12-07 23:11:31 +0900

    캔버스 초기화

--- sketch.js
+++ sketch.js
@@ -1,7 +1,11 @@
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(windowWidth, windowHeight); // 창 크기에 맞는 캔버스 생성
 }
 
 function draw() {
-  background(220);
+  background(240); // 흰색 배경
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight); // 창 크기 변경 시 캔버스 크기 조정
 }
