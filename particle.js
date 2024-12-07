73cf3ad637ff0bde02b980f7775d422e92e23a2b
minseg <artssam7602@naver.com>
2024-12-07 23:20:37 +0900

    particle 수정

--- particle.js
+++ particle.js
@@ -1,58 +1,23 @@
-let particles = [];
-
-function setup() {
-    createCanvas(windowWidth, windowHeight);
-    for (let i = 0; i < 100; i++) {
-        particles.push({
-            pos: createVector(random(width), random(height)),
-            vel: createVector(random(-1, 1), random(-1, 1)),
-            acc: createVector(0, 0),
-            size: random(5, 10)
-        });
+class Particle {
+    constructor(position) {
+        this.pos = position.copy();
+        this.vel = createVector(random(-1, 1), random(-1, 1));
+        this.acc = createVector(0, 0);
+        this.size = random(5, 10);
     }
-}
 
-function draw() {
-    background(240);
-    particles.forEach(p => {
-        // 파티클 속도와 위치 업데이트
-        p.vel.add(p.acc);
-        p.pos.add(p.vel);
-        p.acc.mult(0);
+    applyForce(force) {
+        this.acc.add(force); // 외부에서 받은 힘 적용
+    }
 
-        // 화면 밖으로 나가면 위치 초기화
-        if (p.pos.x < 0 || p.pos.x > width || p.pos.y < 0 || p.pos.y > height) {
-            p.pos = createVector(random(width), random(height));
+    update() {
+        this.vel.add(this.acc);
+        this.pos.add(this.vel);
+        this.acc.mult(0); // 가속도 초기화
+
+        // 화면 밖으로 나가면 재설정
+        if (this.pos.x < 0 || this.pos.x > width || this.pos.y < 0 || this.pos.y > height) {
+            this.pos = createVector(random(width), random(height));
         }
     }
-
-        // 파티클 그리기
-        noStroke();
-        fill(150, 200, 255, 150);
-        ellipse(p.pos.x, p.pos.y, p.size));
-    
-        
-            particles.forEach(p => {
-                // 바람 힘 적용
-                p.acc.add(wind);
-        
-                // 파티클 속도와 위치 업데이트
-                p.vel.add(p.acc);
-                p.pos.add(p.vel);
-                p.acc.mult(0);
-        
-                // 화면 밖으로 나가면 위치 초기화
-                if (p.pos.x < 0 || p.pos.x > width || p.pos.y < 0 || p.pos.y > height) {
-                    p.pos = createVector(random(width), random(height));
-                }
-        
-                // 파티클 그리기
-                noStroke();
-                fill(150, 200, 255, 150);
-                ellipse(p.pos.x, p.pos.y, p.size);
-            });
-        
-            // 바람 초기화
-            wind.mult(0);
-        };
-
+}
